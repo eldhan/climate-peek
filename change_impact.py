4364bd3ae888e8afbe4fd2a17984386e1edbb570
SophieTo <tovagliari.sophie@gmail.com>
2025-01-30 15:09:59 +0100

    adding a filter

--- change_impact.py
+++ change_impact.py
@@ -34,7 +34,7 @@ st.plotly_chart(fig)
 
 
 
-st.subheader("Les sécheresses")
+st.subheader("Les évènements climatiques extrêmes vs les émissions de CO²")
 # Load dataset
 df1, metadata1 = load_dataset("annual-co2-including-land-use")
 df2, metadata2 = load_dataset("number-of-natural-disaster-events")
@@ -44,25 +44,29 @@ df1 = df1.sort_values(by="Year", ascending=True)
 df1 = df1[df1["Code"].notna()]
 df1 = df1[df1["Entity"] == "World"]
 
-df2 = df2[df2["Entity"] == "Drought"]
+df2 = df2[(df2["Entity"] == "Flood") | (df2["Entity"] == "Drought") | (df2["Entity"] == "Wildfire") | (df2["Entity"] == "Extreme temperature") | (df2["Entity"] == "Extreme weather")]
+df_filter = st.selectbox(
+    label="Sélectionnez un filtre : ", options=df2["Entity"].drop_duplicates()
+)
 
 # Create figure with secondary y-axis
 fig2 = make_subplots(specs=[[{"secondary_y": True}]])
 
 # Add traces
 fig2.add_trace(
-    go.Scatter(x= df1["Year"], y= df1["emissions_total_including_land_use_change"], name= "CO²", line=dict(color="blue")),
+    go.Scatter(x= df1["Year"], y= df1["emissions_total_including_land_use_change"], name= "émissions de co²", line=dict(color="blue")),
     secondary_y=False,
 )
 
-fig2.add_trace(
-    go.Scatter(x=df2["Year"], y= df2["n_events"], name= "sécheresses", line=dict(color="red")),
-    secondary_y=True,
-)
+if df_filter:
+  fig2.add_trace(
+      go.Scatter(x=df2[df2["Entity"] == df_filter]["Year"], y= df2[df2["Entity"] == df_filter]["n_events"], name= "nombre d'évènements extrêmes", line=dict(color="red")),
+      secondary_y=True,
+  )
 
 # Add figure title
 fig2.update_layout(
-    title_text="Évolution du nombre de sécheresses et des émissions de CO²"
+    title_text="Évolution du nombre d'évènements climatiques extrêmes et des émissions de CO²"
 )
 
 # Set x-axis title
@@ -70,6 +74,6 @@ fig2.update_xaxes(title_text="Year")
 
 # Set y-axes titles
 fig2.update_yaxes(title_text="émissions de CO2", secondary_y=False)
-fig2.update_yaxes(title_text="sécheresses", secondary_y=True)
+fig2.update_yaxes(title_text="évènements extrêmes", secondary_y=True)
 
 st.plotly_chart(fig2)
